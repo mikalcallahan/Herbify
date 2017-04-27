323a7a19c8f2e5cd8622704bea62477da6032de5
mikal <mikal.callahan@icloud.com>
2017-04-27 10:47:57 -0700

    first commit - getting the repo set up

---
+++ Herbify/Herbify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Herbify
+//
+//  Created by Mikal Callahan on 4/27/17.
+//  Copyright © 2017 Mikal Callahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
